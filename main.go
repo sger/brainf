4552c270cd300b30cb838b5970fd5967bd676d94
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-08-12 12:06:00 +0300

    updating command line

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-	args := os.Args
+	/*args := os.Args
 	if len(args) != 2 {
 		fmt.Printf("Usage: %s filename\n", args[0])
 		return
@@ -19,5 +21,41 @@ func main() {
 		return
 	}
 
-	fmt.Println(string(data))
+	fmt.Println(string(data))*/
+
+	app := cli.NewApp()
+	app.Name = "Brainf"
+	app.Version = "0.0.1"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "file",
+			Aliases: []string{"f"},
+			Usage:   "open a file with extension .bf",
+			Action: func(c *cli.Context) error {
+
+				if len(c.Args().First()) > 0 {
+					data, err := ioutil.ReadFile(c.Args().First())
+					if err != nil {
+						fmt.Printf("Cannot open file %s\n", c.Args().First())
+						return nil
+					}
+
+					fmt.Println(string(data))
+				}
+				return nil
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "Add",
+			Action: func(c *cli.Context) error {
+
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
